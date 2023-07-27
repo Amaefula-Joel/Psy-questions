cedc1a8825ba23802fe063affcda44b6eb48e21a
Joel Amaefula <joelamaefula@gmail.com>
2023-07-27 19:16:41 +0100

    make questions based on programming and print career advise based on answers

--- app.js
+++ app.js
@@ -4,25 +4,54 @@ const app = new Vue({
     data: {
         questions: [
             {
-                text: 'Question 1: What is your favorite color?',
-                name: 'color',
-                options: ['Red', 'Blue', 'Green'],
-                selectedOption: null
-            },
-            {
-                text: 'Question 2: Which programming language do you like?',
+                text: 'Which programming language is commonly used for building web applications?',
                 name: 'language',
-                options: ['JavaScript', 'Python', 'Java'],
+                options: ['Java', 'Python', 'JavaScript', 'C++'],
                 selectedOption: null
             },
             {
-                text: 'Question 3: What is your favorite animal?',
-                name: 'animal',
-                options: ['Dog', 'Cat', 'Bird'],
+                text: 'Which data structure would you use to implement a Last-In-First-Out (LIFO) behavior?',
+                name: 'data-structure',
+                options: ['Queue', 'Linked List', 'Stack', 'Hash Table'],
+                selectedOption: null
+            },
+            {
+                text: 'What is the purpose of version control systems in programming?',
+                name: 'version-control-purpose',
+                options: ['To optimize database performance', 'To manage and track changes in code', 'To test software applications', 'To write complex algorithms'],
+                selectedOption: null
+            },
+            {
+                text: 'Which software development methodology emphasizes incremental development and frequent customer feedback?',
+                name: 'methodology',
+                options: ['Agile', 'Waterfall', 'Scrum', 'RAD (Rapid Application Development)'],
+                selectedOption: null
+            },
+            {
+                text: 'What is the correct syntax to declare a variable in Python?',
+                name: 'python-syntax',
+                options: ['var x = 10', 'int x = 10', 'x = 10', 'let x = 10'],
                 selectedOption: null
             }
         ],
-        currentQuestionIndex: 0
+        currentQuestionIndex: 0,
+        answers: [],
+        suggestCareerOptions: [
+            {
+                selectedOptions : ['JavaScript', 'Stack', 'To manage and track changes in code', 'Agile', 'x = 10'],
+                career: 'Full-Stack Web Developer, DevOps Engineer, Agile Software Developer'
+            },
+            {
+                selectedOptions : ['Java', 'Hash Table', 'To manage and track changes in code', 'Agile', 'x = 10'],
+                career: 'Java Developer, Data Engineer, Agile Software Developer'
+            },
+            {
+                selectedOptions : ['C++', 'Linked List', 'To manage and track changes in code', 'Scrum', 'int x = 10'],
+                career: 'C++ Developer, Software Engineer (General), Scrum Master'
+            }
+        ],
+        // allItemsMatch: null,
+        printedSuggestion: ''
     },
     methods: {
         nextQuestion() {
@@ -32,9 +61,39 @@ const app = new Vue({
             } else {
                 alert('Please select an option before proceeding.');
             }
+
+            // checks to see if the last question has been answered
+            if(this.currentQuestionIndex >= this.questions.length) {
+                this.answers = this.questions.map(question => question.selectedOption);
+
+                this.suggestCareerOptions.some((arr, index) =>{
+                    if (this.answerMatch(arr, this.answers)) {
+                        this.printedSuggestion = this.suggestCareerOptions[index].career;
+                    } else{
+                        this.printedSuggestion = 'Machine Learning Engineer';
+                    }
+                    
+                });
+                console.log(this.printedSuggestion);
+            }
         },
         selectOption(index, item) {
             this.questions[index].selectOption = item;
+        },
+        answerMatch(arr1, arr2) {
+            if (arr1.selectedOptions.length !== arr2.length) {
+                return false;
+            }
+            
+            for (let i = 0; i < arr1.selectedOptions.length; i++) {
+                if (arr1.selectedOptions[i] !== arr2[i]) {
+                    return false;
+                }
+            }
+            
+            return true;
         }
     }
 });
+
+
